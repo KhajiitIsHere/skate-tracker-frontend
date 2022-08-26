a0a6a4b4c423b7cad1d09d06aaa317057ad5c080
Shijakoof <sijakov.filip@gmail.com>
2022-08-26 16:30:32 +0200

    deff final commit

--- src/components/notes/NoteForm.js
+++ src/components/notes/NoteForm.js
@@ -3,9 +3,15 @@ import React, {useState} from "react";
 const NoteForm = ({onSaveNote, onCreateNote, viewingNote}) => {
 
     const [note, setNote] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
     const saveNoteHandler = (event) => {
         event.preventDefault();
+
+        if (note === ''){
+            setErrorMessage('Enter some text before submitting');
+            return;
+        }
 
         onSaveNote(note);
 
@@ -13,11 +19,13 @@ const NoteForm = ({onSaveNote, onCreateNote, viewingNote}) => {
     }
 
     const changeNoteHandler = (event) => {
+        setErrorMessage('');
         setNote(event.target.value);
     }
 
     return (
-        <div className="col">
+        <div className="col mb-3">
+            {errorMessage !== '' && <p className='text-danger mb-2'>{errorMessage}</p>}
             <form onSubmit={saveNoteHandler}>
                 <textarea disabled={viewingNote !== null}  value={viewingNote === null ? note : viewingNote.text} onChange={changeNoteHandler} className='d-block mb-3' rows='8' cols='65'/>
                 {viewingNote === null && <button type="submit" className="btn btn-dark">Save note</button>}
